b534a1a990eff710465701e4e08d2838a5bdec61
jppaschoalini <joao.paschoalini@sou.unaerp.edu.br>
2024-12-01 21:43:20 -0300

    tela de login levando para a tela de cardapio e a tela de cardapio podendo levar para tela de pedidos e de volta para a tela de login

--- app/src/main/java/com/example/trabalho_de_mobile/view/CardapioActivity.kt
+++ app/src/main/java/com/example/trabalho_de_mobile/view/CardapioActivity.kt
@@ -1,13 +1,20 @@
 package com.example.trabalho_de_mobile.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.trabalho_de_mobile.MainActivity
 import com.example.trabalho_de_mobile.R
+import com.example.trabalho_de_mobile.databinding.ActivityCardapioBinding
 
 class CardapioActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCardapioBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +23,19 @@ class CardapioActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding = ActivityCardapioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logout.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.pedido.setOnClickListener{
+            val intent = Intent(this, PedidosActivity::class.java)
+            startActivity(intent)
         }
     }
 }
